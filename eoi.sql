0880b6092d19fc8bc5177c30bfd0179213239502
sammie <106257239@studnt.swin.edu.au>
2025-10-15 14:00:24 +1100

    added eoi.sql and shanged post location for apply form

---
+++ eoi.sql
@@ -0,0 +1,71 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: localhost
+-- Generation Time: Oct 15, 2025 at 04:58 AM
+-- Server version: 10.4.28-MariaDB
+-- PHP Version: 8.2.4
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `webtecg_assignment_2`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `eoi`
+--
+
+CREATE TABLE `eoi` (
+  `id` int(11) NOT NULL,
+  `reference_no` varchar(5) NOT NULL,
+  `firstname` varchar(20) NOT NULL,
+  `lastname` varchar(20) NOT NULL,
+  `dob` date NOT NULL,
+  `gender` enum('Male','Female','prefer_not_to_say') NOT NULL,
+  `email` varchar(50) NOT NULL,
+  `p/n` int(15) NOT NULL,
+  `adress` varchar(40) NOT NULL,
+  `suburb` varchar(20) NOT NULL,
+  `postcode` int(4) NOT NULL,
+  `state` enum('vic','nsw','qld','nt','wa','sa','tas','act') NOT NULL,
+  `skills` set('frontend','backend','database','dataanalysis','projectmanagement') NOT NULL,
+  `other skills` text NOT NULL,
+  `status` enum('New','Current','Final') NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `eoi`
+--
+ALTER TABLE `eoi`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `eoi`
+--
+ALTER TABLE `eoi`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
---
+++ process_eoi.php
@@ -0,0 +1,4 @@
+require_once('settings.php');
+<?php
+
+?>
