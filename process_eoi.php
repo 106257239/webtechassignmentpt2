47dab26c6dc5cafe3777725871da0f064462cb02
sammie <106257239@studnt.swin.edu.au>
2025-10-16 22:10:42 +1100

    added to process_eoi.php

--- process_eoi.php
+++ process_eoi.php
@@ -5,11 +5,24 @@ require_once('settings.php');
     if (!$conn) {
         die("Connection failed: " . mysqli_connect_error());
     $results = mysqli_select_db($conn, "db_name");}
-        if ($results) {}
-            echo "conection sucessfull"
-        require_once ('eoi.sql");
+        $job_reference = $_post['job_reference'];
+        $firstname = $_POST['firstname'];
+        $lastname = $_POST['lastname'];
+        $dob = $_POST['DOB'];
+        $gender = $_POST['gender'];
+        $email = $_POST['email'];
+        $phone = $_POST['phone'];
+        $address = $_POST['address'];
+        $suburb = $_POST['suburb'];
+        $postcode = $_POST['postcode'];
+        $state = $_POST['state'];
 
-    // INSERT INTO `eoi` (`id`, `reference_no`, `firstname`, `lastname`, `dob`, `gender`, `email`, `p/n`, `adress`, `suburb`, `postcode`, `state`, `skills`, `other skills`, `status`) 
-    // VALUES (NULL, '', '', '', '', '', '', '', '', '', '', '', '', '', '')
+        $frontend= $_POST['Frontend'];
+        $otherskills = $_POST['suburb'];
+        echo "email: " . htmlspecialchars($email) . "<br>";
+        echo "phone: " . htmlspecialchars($phone);
+    INSERT INTO `eoi` (`id`, `reference_no`, `firstname`, `lastname`, `dob`, `gender`, `email`, `p/n`, `adress`, `suburb`, `postcode`, `state`, `skills`, `other skills`, `status`) 
+    VALUES (NULL, ,'$job_refrence','$firstname', '$lastname', '$DOB', '$gender', 
+    '$email', '$phone', '$address', '$suburb', '$postcode', 'state','$frontend', '$otherskills', NULL,)
 ?>
 hi
