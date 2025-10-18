0de9548e861d66ab1090fd9c55b68f8865c23a11
sammie <106257239@studnt.swin.edu.au>
2025-10-18 18:59:14 +1100

    got sql and php working together

--- process_eoi.php
+++ process_eoi.php
@@ -1,13 +1,20 @@
-
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
 <?php
-require_once('settings.php');
-    $conn = mysqli_connect("localhost", "root", "", "web_tech_assignment_2");
-    if (!$conn) {
-        die("Connection failed: " . mysqli_connect_error());
-    $results = mysqli_select_db($conn, "db_name");}
-        $job_reference = $_post['job_reference'];
-        $firstname = $_POST['firstname'];
-        $lastname = $_POST['lastname'];
+
+?>
+<?php
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+        $job_reference = $_POST['job_reference'];
+        $firstname = $_POST['first_name'];
+        $lastname = $_POST['last_name'];
         $dob = $_POST['DOB'];
         $gender = $_POST['gender'];
         $email = $_POST['email'];
@@ -16,12 +23,51 @@ require_once('settings.php');
         $suburb = $_POST['suburb'];
         $postcode = $_POST['postcode'];
         $state = $_POST['state'];
-
         $frontend= $_POST['Frontend'];
         $otherskills = $_POST['suburb'];
-        echo "email: " . htmlspecialchars($email) . "<br>";
-        echo "phone: " . htmlspecialchars($phone);
-    // INSERT INTO `eoi`(`id`, `reference_no`, `firstname`, `lastname`, `dob`, `gender`, `email`, `p/n`, `adress`, `suburb`, `postcode`, `state`, `skills`, `other skills`, `status`) 
-    // VALUES (NULL, ,'$job_refrence','$firstname', '$lastname', '$DOB', '$gender', $email', '$phone', '$address', '$suburb', '$postcode', 'state','$frontend', '$otherskills', NULL,)
+            // variable gathering testing
+            echo $job_reference;
+            echo $firstname;
+            echo $lastname;
+            echo $dob;
+            echo $gender;
+            echo $email;
+            echo $phone;
+            echo $address;
+            echo $suburb;
+            echo $postcode;
+            echo $state;
+            echo $frontend;
+            echo $otherskills;
+        }
+// require_once('settings.php');
+$host = "localhost";
+$user = "root";
+$pwd = "";
+$sql_db = "webtech_assignment_2"; // Replace with your actual DB name
+
+// connect to database
+$conn = @mysqli_connect($host, $user, $pwd, $sql_db);
+
+// Check connection
+if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
+} else {
+    echo "Connected successfully<br>";
+}
+
+$sql = "INSERT INTO `eoi` (`id`, `reference_no`, `firstname`, `lastname`, `dob`, `gender`, `email`,`phone`, `address`, `suburb`, `postcode`, `state`, `skills`, `otherskills`, `status`)
+VALUES (Null, '$job_reference', '$firstname', '$lastname', '$dob', '$gender', '$email', '$phone', '$address', '$suburb', '$postcode', '$state', '$frontend', '$otherskills', 1 );";
+$results = mysqli_query($conn, $sql);
+
+if ($results) {
+    echo "New record created successfully";
+} else {
+    echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+}
+
+mysqli_close($conn);
 ?>
 hi
+</body>
+</html>
