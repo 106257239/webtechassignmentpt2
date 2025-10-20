95d1fc5c0690aed0c041bc2d55ffa81579758c29
VethumHelith <105218578@swin.edu.au>
2025-10-21 00:06:28 +1100

    Delete settings.php

--- settings.php
+++
@@ -1,7 +0,0 @@
-<?php
-// Database connection settings for local XAMPP server
-$host = "localhost";   // your database host
-$user = "root";        // your MySQL username
-$pwd  = "";            // leave blank (no password in XAMPP)
-$sql_db = "project2_db";  // your database name
-?>
