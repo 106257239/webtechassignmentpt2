410b58937564fa8e917b72dd17a85c9e4b538e71
VethumHelith <105218578@swin.edu.au>
2025-10-20 23:51:46 +1100

    made settings.php  , This file stores your MySQL database connection details.

---
+++ settings.php
@@ -0,0 +1,7 @@
+<?php
+// Database connection settings for local XAMPP server
+$host = "localhost";   // your database host
+$user = "root";        // your MySQL username
+$pwd  = "";            // leave blank (no password in XAMPP)
+$sql_db = "project2_db";  // your database name
+?>
