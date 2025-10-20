1881c53c6086129fc14ca4668ae30e0a8ca88694
sammie <106257239@studnt.swin.edu.au>
2025-10-20 12:29:33 +1100

    Added table creation if eoi table if table does not exist

--- process_eoi.php
+++ process_eoi.php
@@ -1,9 +1,16 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+<meta charset="UTF-8">
+        <meta name = "viewport" content = "width=device-width, initial-scale=1.0">
+        <meta name = "description" content = "job aplication form data ">
+        <meta name = "keywords" content = "process, swinburne, assignment">
+        <meta name = "author" content = "Sammie Miller">
+        <link rel="icon" type="image/x-icon" href="styles/images/shrimp.jpg">
+        <!--CSS Refrencing-->
+        <link href="styles/style.css"rel= "stylesheet">
+        <!--page tab title-->
+        <title>Form Results</title>
 </head>
 <body>
 <?php
@@ -14,18 +21,73 @@ if ($_SERVER["REQUEST_METHOD"] != "POST") {
 // retrieve from post method form data and assign to variable 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
         $job_reference = trim($_POST['job_reference']);
-        $firstname = trim($_POST ['first_name']);
-        $lastname = trim($_POST['last_name']);
-        $dob = trim($_POST['DOB']);
-        $gender = trim($_POST['gender']);
-        $email = trim($_POST['email']);
-        $phone = trim($_POST['phone']);
-        $address = trim($_POST['address']);
-        $suburb = trim($_POST['suburb']);
-        $postcode = trim($_POST['postcode']);
-        $state = trim($_POST['state']);
-        $frontend= trim($_POST['Frontend']);
-        $otherskills = trim($_POST['other_skills']);
+        $firstname = sanitise_data($_POST ['first_name']);
+        $lastname = sanitise_data($_POST['last_name']);
+        $dob = sanitise_data($_POST['DOB']);
+        $gender = sanitise_data($_POST['gender']);
+        $email = sanitise_data($_POST['email']);
+        $phone = sanitise_data($_POST['phone']);
+        $address = sanitise_data($_POST['address']);
+        $suburb = sanitise_data($_POST['suburb']);
+        $postcode = sanitise_data($_POST['postcode']);
+        $state = sanitise_data($_POST['state']);
+        $frontend= sanitise_data($_POST['Frontend']);
+        $otherskills = sanitise_data($_POST['other_skills']);
+        // retrieve required information for connection
+require_once('settings.php');
+
+// connect to database
+$conn = @mysqli_connect($host, $user, $pwd, $sql_db);
+
+// Check connection if it was a failure or success
+if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
+} else {
+    echo "Connected successfully<br>";}
+
+    $tablename = "eoi";
+    $createTableSQL = "CREATE TABLE IF NOT EXISTS $tablename (
+        id INT(11) NOT NULL AUTO_INCREMENT,
+        reference_no VARCHAR(5) NOT NULL,
+        firstname VARCHAR(20) NOT NULL,
+        lastname VARCHAR(20) NOT NULL,
+        dob DATE NOT NULL,
+        gender ENUM('Male','Female','prefer_not_to_say') NOT NULL,
+        email VARCHAR(50) NOT NULL,
+        phone INT(15) NOT NULL,
+        address VARCHAR(40) NOT NULL,
+        suburb VARCHAR(20) NOT NULL,
+        postcode INT(4) NOT NULL,
+        state ENUM('vic','nsw','qld','nt','wa','sa','tas','act') NOT NULL,
+        skills SET('frontend','backend','database','dataanalysis','projectmanagement') NOT NULL,
+        otherskills TEXT NOT NULL,
+        status ENUM('New','Current','Final') NOT NULL DEFAULT 'New',
+        created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+        PRIMARY KEY (id)
+    ) ENGINE=InnoDB;";
+
+    if (!mysqli_query($conn, $createTableSQL)) {
+        die(" Error creating table: " . mysqli_error($conn));
+    } else {
+        echo " Table '$tablename' ready.<br>";
+    }
+
+
+//insert data from variables in to eoi table 
+$stmt = $conn->prepare  ("INSERT INTO `eoi` (`id`, `reference_no`, `firstname`, `lastname`, `dob`, `gender`, `email`,`phone`, `address`, `suburb`, `postcode`, `state`, `skills`, `otherskills`, `status`)
+VALUES (Null, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, 1 );");
+$stmt->bind_param("sssssssssssss",  $job_reference,$firstname,$lastname,$dob,$gender,$email,$phone,$address,$suburb,$postcode,$state,$frontend,$otherskills);
+$stmt->execute();
+$results = $stmt->get_result();
+// echo if results where successful
+if ($stmt) {
+    echo "New record created successfully";
+} else {
+    echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+}
+// close sql connections 
+mysqli_close($conn);
+
         // variable displaying 
             echo $job_reference . "<br>";
             echo $firstname. "<br>";
@@ -41,32 +103,13 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
             echo $frontend. "<br>";
             echo $otherskills. "<br>";
         }
-// retrieve required information for connection
-require_once('settings.php');
-
-// connect to database
-$conn = @mysqli_connect($host, $user, $pwd, $sql_db);
-
-// Check connection if it was a failure or success
-if (!$conn) {
-    die("Connection failed: " . mysqli_connect_error());
-} else {
-    echo "Connected successfully<br>";
+//Data sanatising 
+function sanitise_data($data) {
+    $data = trim($data);                 
+    $data = htmlspecialchars($data);
+    $data = stripslashes($data);       
+    return $data;
 }
-// insert data from variables in to eoi table 
-$stmt = $conn->prepare  ("INSERT INTO `eoi` (`id`, `reference_no`, `firstname`, `lastname`, `dob`, `gender`, `email`,`phone`, `address`, `suburb`, `postcode`, `state`, `skills`, `otherskills`, `status`)
-VALUES (Null, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, 1 );");
-$stmt->bind_param("sssssssssssss",  $job_reference,$firstname,$lastname,$dob,$gender,$email,$phone,$address,$suburb,$postcode,$state,$frontend,$otherskills);
-$stmt->execute();
-$results = $stmt->get_result();
-// echo if results where successful
-if ($stmt) {
-    echo "New record created successfully";
-} else {
-    echo "Error: " . $sql . "<br>" . mysqli_error($conn);
-}
-// close sql connections 
-mysqli_close($conn);
 ?>
 </body>
 </html>
