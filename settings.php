d2336b6657ac91896d4013ba7742d80880259ffd
vethum helith hewapathirana <105218578@student.swin.edu.au>
2025-10-27 19:01:48 +1100

    new settings

--- settings.php
+++ settings.php
@@ -2,5 +2,5 @@
 $host = "localhost";               // XAMPP server
 $user = "root";                    // default username for XAMPP
 $pwd  = "";                        // leave empty unless you set a MySQL password
-$sql_db = "webtech2";  // ✅ updated database name
+$sql_db = "webtech_assignment_2";  // ✅ updated database name
 ?>
